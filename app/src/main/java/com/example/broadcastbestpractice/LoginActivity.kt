346f6c428bee3b21e6cb87e567963c77a96e286a
LGC <1621853164@qq.com>
2020-11-16 10:56:44 +0800

    修改了APP图标

--- app/src/main/java/com/example/broadcastbestpractice/LoginActivity.kt
+++ app/src/main/java/com/example/broadcastbestpractice/LoginActivity.kt
@@ -1,11 +1,25 @@
 package com.example.broadcastbestpractice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        LoginButton.setOnClickListener {
+            val id = UserID.text.trim().toString()
+            val pwd = UserPwd.text.trim().toString()
+            if (id == "1621853164" && pwd == "123456") {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this,"QQ账号或密码错误",Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
