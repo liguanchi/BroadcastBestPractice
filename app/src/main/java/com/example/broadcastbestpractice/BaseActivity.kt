4e95fd9bf9dbee079bab5f02a706bcf984708cdd
LGC <1621853164@qq.com>
2020-11-24 09:14:00 +0800

    完成记住密码功能

--- app/src/main/java/com/example/broadcastbestpractice/BaseActivity.kt
+++ app/src/main/java/com/example/broadcastbestpractice/BaseActivity.kt
@@ -8,7 +8,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
-open class BaseActivity: AppCompatActivity() {
+open class BaseActivity : AppCompatActivity() {
     lateinit var receiver: BroadcastReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +24,7 @@ open class BaseActivity: AppCompatActivity() {
         super.onResume()
         val intentFilter = IntentFilter("com.example.broadcastbestpractice.FORCS_OFFLINE")
         receiver = ForceOfflineReceiver()
-        registerReceiver(receiver,intentFilter)
+        registerReceiver(receiver, intentFilter)
     }
 
     /**
@@ -40,20 +40,32 @@ open class BaseActivity: AppCompatActivity() {
         ActivityCollector.removeActivity(this)
     }
 
-    inner class ForceOfflineReceiver:BroadcastReceiver(){
+    inner class ForceOfflineReceiver : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
-           AlertDialog.Builder(context).apply {
-               setTitle("强制下线通知")
-               setMessage("您已被下线，如果非本人操作，请尽快更改密码")
-               setCancelable(false)
-               setPositiveButton("确定"){_,_->
-                   //重新开启登录界面
-                   ActivityCollector.finishAll()
-                   val i = Intent(context,LoginActivity::class.java)
-                   context.startActivity(i)
-               }
-               show()
-           }
+            AlertDialog.Builder(context).apply {
+                setTitle("强制下线通知")
+                setMessage("您已被下线，如果非本人操作，请尽快更改密码")
+                setCancelable(false)
+                setPositiveButton("确定") { _, _ ->
+                    //重新开启登录界面
+                    ActivityCollector.finishAll()
+                    val i = Intent(context, LoginActivity::class.java)
+                    context.startActivity(i)
+                }
+                show()
+            }
+
+//            val a = AlertDialog.Builder(context)
+//            a. setTitle("强制下线通知")
+//            a. setMessage("您已被下线，如果非本人操作，请尽快更改密码")
+//            a.setCancelable(false)
+//            a. setPositiveButton("确定") { _, _ ->
+//                //重新开启登录界面
+//                ActivityCollector.finishAll()
+//                val i = Intent(context, LoginActivity::class.java)
+//                context.startActivity(i)
+//            }
+//            a.show()
         }
     }
 
--- app/src/main/java/com/example/broadcastbestpractice/LoginActivity.kt
+++ app/src/main/java/com/example/broadcastbestpractice/LoginActivity.kt
@@ -1,19 +1,45 @@
 package com.example.broadcastbestpractice
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.Toast
+import androidx.core.content.edit
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val sp =  getSharedPreferences("sp_pwd", Context.MODE_PRIVATE)
+        val isRemember = sp.getBoolean("remember_pwd",false)
+        if (isRemember){
+            val id = sp.getString("user_id","")
+            val pwd = sp.getString("user_pwd","")
+            UserID.setText(id)
+            UserPwd.setText(pwd)
+            RememberPwd.isChecked = true
+        }
+
+
         LoginButton.setOnClickListener {
             val id:CharSequence=UserID.text.trim().toString()
             val pwd:String=UserPwd.text.trim().toString()
             if (id=="1621853164"&&pwd=="123456"){
+                val sp = getSharedPreferences("sp_pwd", Context.MODE_PRIVATE).edit()
+                if (RememberPwd.isChecked){
+                    getSharedPreferences("sp_pwd", Context.MODE_PRIVATE)
+                        sp.putBoolean("remember_pwd",true)
+                        sp.putString("user_id", id.toString())
+                        sp.putString("user_pwd", pwd)
+                }else{
+                    sp.clear()
+                }
+                sp.apply()
                 val intent= Intent(this,MainActivity::class.java)
                 startActivity(intent)
                 finish()
@@ -21,5 +47,12 @@ class LoginActivity : BaseActivity() {
                 Toast.makeText(this,"QQ号码或密码错误,请重试",Toast.LENGTH_LONG).show()
             }
         }
+
+
+
+
+
+
+
     }
 }
